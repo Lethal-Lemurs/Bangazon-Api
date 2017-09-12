ddd08c9ccc648f4ddad40a357f9f7fb92ec849c2
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-09-12 15:14:45 -0500

    added user table

--- db/build-db.js
+++ db/build-db.js
@@ -1,27 +1,29 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('bangazonStore.sqlite');
 
-const { productTypes } = require('../data/product-types');
+// const { productTypes } = require('../data/product-types');
 const { employeeTypes } = require('../data/employee-types');
 
-const { generateEmployees } = require('../data/employees');
-const { generateDepartments } = require('../data/departments');
-const { generatePrograms } = require('../data/programs');
-const { generateComputers } = require('../data/computers');
+const { generateEmployees } = require('../data/employees-data');
+const { generateDepartments } = require('../data/departments-data');
+const { generatePrograms } = require('../data/programs-data');
+const { generateComputers } = require('../data/computers-data');
+const { generateUsers } = require('../data/users-data');
 
 let employees = generateEmployees(employeeTypes);
-
 let departments = generateDepartments();
-
 let programs = generatePrograms();
-
 let computers = generateComputers();
+
+let user = generateUsers();
+console.log('users', user);
 
 db.serialize(function() {
   db.run(`DROP TABLE IF EXISTS employees`);
   db.run(`DROP TABLE IF EXISTS departments`);
   db.run(`DROP TABLE IF EXISTS programs`);
   db.run(`DROP TABLE IF EXISTS computers`);
+  db.run(`DROP TABLE IF EXISTS user`);
 
   db.run(`CREATE TABLE IF NOT EXISTS employees (
     emp_id INTEGER NOT NULL PRIMARY KEY,
@@ -53,25 +55,43 @@ db.serialize(function() {
       FOREIGN KEY (employee_id) REFERENCES employees(emp_id) )`
   );
 
+  // ******* USER TABLES
+  db.run(`CREATE TABLE IF NOT EXISTS user (
+    user_id INTEGER NOT NULL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    phone INT NOT NULL,
+    email TEXT NOT NULL,
+    address_street TEXT NOT NULL,
+    address_city TEXT NOT NULL,
+    address_state TEXT NOT NULL,
+    address_zip INT NOT NULL)`
+  );
 
+  
   employees.forEach( ({first_name, last_name, phone, position_title }) => {
     db.run(`INSERT INTO employees (first_name, last_name, phone, position_title)
-      VALUES ("${first_name}", "${last_name}", ${phone}, "${position_title}") `);
+    VALUES ("${first_name}", "${last_name}", ${phone}, "${position_title}") `);
   });
-
+  
   departments.forEach( ({dept_name, expense_budget}) => {
     db.run(`INSERT INTO departments (dept_name, expense_budget)
-      VALUES ("${dept_name}", ${expense_budget}) `);
+    VALUES ("${dept_name}", ${expense_budget}) `);
   });
-
+  
   programs.forEach( ({start_date, end_date}) => {
     db.run(`INSERT INTO programs (start_date, end_date)
-      VALUES ("${start_date}", "${end_date}") `);
+    VALUES ("${start_date}", "${end_date}") `);
   });
-
+  
   computers.forEach( ({purchased_date}) => {
     db.run(`INSERT INTO computers (purchased_date)
-      VALUES ("${purchased_date}")`);
+    VALUES ("${purchased_date}")`);
   });
-
+  
+  // ******* USER TABLES
+  user.forEach( ({first_name, last_name, phone, email, address_street, address_city, address_state, address_zip}) => {
+    db.run(`INSERT INTO user (first_name, last_name, phone, email, address_street, address_city, address_state, address_zip)
+      VALUES ("${first_name}", "${last_name}", ${phone}, "${email}", "${address_street}", "${address_city}", "${address_state}", ${address_zip}) `);
+  });
 });
