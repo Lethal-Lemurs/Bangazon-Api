df1506799ac95fc0b031f048b382cd60a178c95d
Dustin Walker <thewalkman1234@gmail.com>
2017-09-12 13:15:15 -0500

    customers

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const { getAllCustomers, getOneCustomer } = require('../models/customerModel.js');
+const { get_all_customers, get_one_customer } = require('../models/customerModel.js');
 
-module.exports.getCustomers = (req, res, next) => {
-    getAllCustomers()
+module.exports.get_customers = (req, res, next) => {
+    get_all_customers()
 }
 
 
 
-module.exports.getOneCustomer = (req, res, next) => {
+module.exports.get_one_customer = (req, res, next) => {
 
 }
