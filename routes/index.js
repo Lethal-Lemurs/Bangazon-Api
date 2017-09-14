ef936440806ec8dd17461b7eaef0cd8ad9407811
Glen Vollmer <glenvollmer@Glens-Macbook-Pro.local>
2017-09-14 14:14:12 -0500

    fixed a small typo in the index for routes

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.get('/', (req, res) => {
         "prodcutType by id": "api/v1/product-types/id",
         "post new prodcutType": "api/v1/product-types/new",
         "replace a productType with put": "/api/v1/product-types/replace/:id",
-        "delete a productType by id": "/api/v1/products/delete/:id",
+        "delete a productType by id": "/api/v1/products-types/delete/:id",
         // "trainingProgram": "api/v1/training-program"
     });
 });
